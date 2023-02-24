af2f4911d381dc251a224065bf21b57ba619f05c
Sarah Jones <sarahrjones8@gmail.com>
2023-02-24 00:16:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const name = ["Charlie", "Samip", "Ali"];
+const event = "birthday"
+
+function writeCards(name, event) {
+    const message = [];
+    for (let i = 0; i < name.length; i++) { 
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+
+    return message;
+}
+
+writeCards(name, event);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+
+    return number;
+}
+
